38fff442e356f37c539308dd0f65ba385af6abd3
DenCap <dionisiecaprosu@Dionisies-MacBook-Pro.local>
2025-07-28 19:50:40 -0500

    removed greeting from module1

--- roots/devops-project-main/main.tf
+++ roots/devops-project-main/main.tf
@@ -1,11 +1,11 @@
 
 # DO NOT REMOVE DUMMY MODULE references and their code, they should remain as examples
-module "module1" {
-  source = "../../dummy-module-1"
-  # ... any required variables for module1
-  greeting = var.greeting
+# module "module1" {
+#   source = "../../dummy-module-1"
+#   # ... any required variables for module1
+#   greeting = var.greeting
 
-}
+# }
 
 module "module2" {
   source             = "../../dummy-module-2"
